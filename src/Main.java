e45db6f0f7f426c4f175957e88695799ebec86d3
camquevedo <camquevedo@hotmail.com>
2023-11-08 19:26:48 -0500

    :construction: Menu Opciones de App Mi Tienda de Barrio

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,119 @@
+import java.util.Scanner;
+
 public class Main {
-        static class Product {
-            String name;
-            String description;
-            String category;
-            String tags;
-            double price;
-            String imageUrl;
+
+    static Scanner scanner;
+    static class Product {
+        String name;
+        String description;
+        String category;
+        String tags;
+        double price;
+        String imageUrl;
+    }
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
+        Product[] products = new Product[10];
+
+        products[1] = new Product();
+        products[1].name = "Colgate";
+        products[1].price = 15.000f;
+        System.out.println("product name: " + products[1].name);
+
+        products[2] = new Product();
+        products[2].name = "Leche Entera Alpina";
+        products[2].description = "Bolsa x 1.100 ml";
+        products[2].category = "Lacteo/Huevos y refrigerados";
+        products[2].tags = "Leches";
+        products[2].price = 3100;
+        products[2].imageUrl = "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg";
+        
+        runMenu();
+
+        scanner.close();
+    }
+
+    public static void runMenu() {
+        int choice;
+        do {
+            displayMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+            handleUserChoice(choice);
+        } while (choice != 8);
+    }
+
+    private static void displayMenu() {
+
+        System.out.println(
+                """
+                        |o|                         /////////////\\\\\\
+                        |o|                        (((((((((((((   \\\\\\
+                        |o|                        ))) ~~      ~~   (((
+                        |o|                        ((( (*)     (*)  )))
+                        |o|                        )))     <        (((\s
+                        |o|                        ((( '\\______/`   )))\s
+                        |o|                        )))\\___________/(((\s
+                        |o|                        (((   _)  (_    )))\s\s
+                        |o|                              /\\__/\\""");
+        System.out.println("±----------------------------------------±");
+        System.out.println("|   Administrador Mi Tienda de Barrio    |");
+        System.out.println("±----------------------------------------±");
+        System.out.println("1. Agregar producto                      |");
+        System.out.println("2. Eliminar producto                     |");
+        System.out.println("3. Actualizar producto                   |");
+        System.out.println("4. Ver todos los productos               |");
+        System.out.println("5. Ver producto                          |");
+        System.out.println("6. Buscar producto en inventario         |");
+        System.out.println("7. Ver producto destacado                |");
+        System.out.println("8. Salir                                 |");
+        System.out.println("±----------------------------------------±");
+        System.out.print("   Ingresa tu opción:    (1 - 8)  ");
+    }
+
+    private static void handleUserChoice(int choice) {
+        switch (choice) {
+            case 1 -> addProduct();
+            case 2 -> removeProduct();
+            case 3 -> updateProduct();
+            case 4 -> listAllProducts();
+            case 5 -> listProduct();
+            case 6 -> searchProduct();
+            case 7 -> showHighlightProduct();
+            case 8 -> System.out.println("Saliendo...");
+            default -> System.out.println("Opción invalida. Por favor intenta de nuevo.");
         }
 
-        public static void main(String[] args) {
-            Product[] products = new Product[10];
-            products[1] = new Product();
-            products[1].name = "Colgate";
-            products[1].price = 15.000f;
+    }
 
-            products[2].name = "Leche Entera Alpina";
-            products[2].description = "Bolsa x 1.100 ml";
-            products[2].category = "Lacteo/Huevos y refrigerados";
-            products[2].tags = "Leches";
-            products[2].price = 3100;
-            products[2].imageUrl = "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg";
-            System.out.println("product name: " + products[1].name);
-        }
+    private static void addProduct() {
+        System.out.println("addProduct en ejecución ...Soon");
+    }
+
+    private static void removeProduct() {
+        System.out.println("removeProduct en ejecución ...Soon");
+    }
+
+    private static void updateProduct() {
+        System.out.println("updateProduct en ejecución ...Soon");
+    }
+
+    private static void listProduct() {
+        System.out.println("listProduct en ejecución ...Soon");
+    }
+
+    private static void listAllProducts() {
+        System.out.println("listAllProducts en ejecución ...Soon");
+    }
+
+    private static void searchProduct() {
+        System.out.println("searchProduct en ejecución ...Soon");
+    }
+
+    private static void showHighlightProduct() {
+        System.out.println("showHighlightProduct en ejecución ...Soon");
+    }
+
 }
